9b1734b3ef2ddbbadf89273ac588d0cf949d4f7f
dmytromaimesko <dima.maimesko@gmail.com>
2026-05-15 00:52:45 +0300

    Add followers functionality, user feed API, and supporting migrations and routes

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -19,20 +19,27 @@ type Storage struct {
 		Create(context.Context, *Post) error
 		Delete(context.Context, int64) (int, error)
 		Update(context.Context, *Post) error
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
 	}
 	Users interface {
 		Create(context.Context, *User) error
+		GetByID(context.Context, int64) (*User, error)
 	}
 	Comments interface {
 		Create(context.Context, *Comment) error
 		GetByPostID(context.Context, int64) ([]Comment, error)
 	}
+	Followers interface {
+		Follow(ctx context.Context, followerID, userID int64) error
+		Unfollow(ctx context.Context, followerID, userID int64) error
+	}
 }
 
 func NewPostgresStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts:    &PostStore{db},
-		Users:    &UserStore{db},
-		Comments: &CommentStore{db},
+		Posts:     &PostStore{db},
+		Users:     &UserStore{db},
+		Comments:  &CommentStore{db},
+		Followers: &FollowerStore{db},
 	}
 }
